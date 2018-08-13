10d85e6a3897b180d543ec5dabe83471a9388ba3
Ghataorhe <b.ghataorhe@gmail.com>
2018-08-12 21:29:31 -0700

    import react router

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -2,7 +2,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+
 //import CSS
 import css from './styles/style.styl';
 
-render(<p>hello</p>, document.getElementById('root'));
+//import Components
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+//import react router
+import { Router, Route, IndexRoute, browserHistory }
+
+render(<Main />, document.getElementById('root'));
