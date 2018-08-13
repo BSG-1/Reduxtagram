a272fb90691b8208bb79dfbe8956f980a6cdba7b
Ghataorhe <b.ghataorhe@gmail.com>
2018-08-13 14:18:41 -0700

    imports the default data (comments, posts etc)

--- client/store.js
+++ client/store.js
@@ -2,4 +2,11 @@ import { createStore, compse } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { broswerHistory } from 'react-router';
 
-//import the root reduce
+//import the root reducer
+import rootReducer from './reducers/index';
+
+//default data
+import comments from './data/comments';
+import comments from './data/posts';
+
+//create an object for the default data
