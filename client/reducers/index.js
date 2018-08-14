f93c7e55680748dc1dbb627b4a4f8cd7886dd440
Ghataorhe <b.ghataorhe@gmail.com>
2018-08-14 13:09:01 -0700

    creates the rootreducer called 'index' which imports comments and posts in order to send a combinedreducer to the redux store

---
+++ client/reducers/index.js
@@ -0,0 +1,9 @@
+//this is the rootreducer
+
+import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+
+import posts from './posts';
+import comments from './comments';
+
+const rootReducer = combineReducers({ posts, comments });
