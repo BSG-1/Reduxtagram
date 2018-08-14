21b81a0e94ca2fd288087722b64217210e4b0923
Ghataorhe <b.ghataorhe@gmail.com>
2018-08-14 13:05:22 -0700

    adds comments.js

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -3,4 +3,9 @@
 //1. the action (info about what happened)
 //2. a copy of the current state
 
-function posts(state = [])
+function posts(state = [], action) {
+    console.log(state, action);
+    return state;
+}
+
+export default posts;
