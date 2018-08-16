e648eff912e5cc29cd03de46306decd06d5d2aff
Ghataorhe <b.ghataorhe@gmail.com>
2018-08-16 15:47:47 -0700

    fixes browserhistory listen problem --> misspelling

--- client/store.js
+++ client/store.js
@@ -1,6 +1,6 @@
 import { createStore, compose } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
-import { broswerHistory } from 'react-router';
+import { browserHistory } from 'react-router';
 
 //import the root reducer
 import rootReducer from './reducers/index';
@@ -17,6 +17,6 @@ const defaultState = {
 
 const store = createStore(rootReducer, defaultState);
 
-// export const history = syncHistoryWithStore(broswerHistory, store);
+export const history = syncHistoryWithStore(browserHistory, store);
 
 export default store;
