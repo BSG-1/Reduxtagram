b434d9771fe6a9f737d2192ae1608166c98534e2
Ghataorhe <b.ghataorhe@gmail.com>
2018-08-19 15:00:37 -0700

    surface all data through state props

--- client/components/App.js
+++ client/components/App.js
@@ -3,11 +3,16 @@ import { connect } from 'react-redux';
 import * as actionCreators from '../actions/actionCreators';
 import Main from './Main';
 
+//surface all data
 function mapStateToProps(state) {
     return {
         posts: state.posts,
         comments: state.comments
     }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(actionCreators, dispatch);
 }
 
 const App = connect(mapStateToProps, mapDispatchToProps);
