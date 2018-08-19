1ea905d934b72c831d835f178da83767fc11b0cb
Ghataorhe <b.ghataorhe@gmail.com>
2018-08-19 15:28:42 -0700

    displayes comments via this.props

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router';
 import Photo from './Photo'
+
+//in order to get the actual element's key, you need to actually give it an i = {i} in React
 const PhotoGrid = React.createClass({
     render() {
         return (
             <div className="photo-grid">
-                {this.props.posts.map((post, i) => <Photo {...this.props} key={i} i={i} />)}
+                {this.props.posts.map((post, i) => <Photo {...this.props} key={i} i={i} post={post} />)}
             </div>
         )
     }
